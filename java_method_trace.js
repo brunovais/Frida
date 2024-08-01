b4038f9f3b069f57655606e41ddeae7a1a8a84e7
4l1f <53953876+brunovais@users.noreply.github.com>
2024-08-01 11:52:46 -0300

    Update java_method_trace.js
    easy to use

--- java_method_trace.js
+++ java_method_trace.js
@@ -2,8 +2,25 @@
  07/2024
  this script shows all methods call chain in trace of one method
  */
-function methodTrace(){
+
+var className = 'com.target.Class';
+var methodName = 'methodTarget';
+
+ function methodTrace(){
     var stackTrace = Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new())
     stackTrace = stackTrace.replace("java.lang.Exception\n", "Method trace: \n")
     console.log(stackTrace)
   }
+
+  Java.perform(function () {
+    var TargetClass = Java.use(className);
+
+    var targetMethod = TargetClass[methodName].overload();
+
+    targetMethod.implementation = function () {
+        methodTrace();
+        var result = this[methodName]();
+
+        return result;
+    };
+});
