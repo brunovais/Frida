bcad766c4bfb2f2acf076dbf978c5f2ee14e9899
4l1f <53953876+brunovais@users.noreply.github.com>
2024-11-19 10:34:39 -0300

    Update call_activity.js

--- call_activity.js
+++ call_activity.js
@@ -3,9 +3,9 @@
         this script call a Activity @android
         by @brunovais
     */
+
     function callActivity(activity) {
         console.log("\n[ + ] Call Activity")
-
         var ActivityThread = Java.use("android.app.ActivityThread");
         var context = ActivityThread.currentApplication().getApplicationContext();
         var Intent = Java.use("android.content.Intent");
@@ -13,5 +13,4 @@
         var intent = Intent.$new(context, MyActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK.value);
         context.startActivity(intent);
-
     }
