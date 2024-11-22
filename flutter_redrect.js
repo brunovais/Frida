3ea6b3b3e12d41e10dfb9632336b80e1dcdc3c27
4l1f <53953876+brunovais@users.noreply.github.com>
2024-11-22 13:44:44 -0300

    Add files via upload

---
+++ flutter_redrect.js
@@ -0,0 +1,37 @@
+// redirect_socket.js
+
+Interceptor.attach(Module.findExportByName(null, 'connect'), {
+    onEnter: function(args) {
+        var sockfd = args[0].toInt32();
+        var addrPtr = args[1];
+        var addrlen = args[2].toInt32();
+
+        var family = Memory.readU16(addrPtr);
+
+        if (family == 2) {
+            var port = Memory.readU16(addrPtr.add(2));
+            var ip = Memory.readU32(addrPtr.add(4));
+
+            port = ((port & 0xFF) << 8) | ((port >> 8) & 0xFF);
+
+            var ipStr = ((ip >> 24) & 0xFF) + '.' +
+                        ((ip >> 16) & 0xFF) + '.' +
+                        ((ip >> 8) & 0xFF) + '.' +
+                        (ip & 0xFF);
+
+            console.log('Conectando para ' + ipStr + ':' + port);
+
+            var proxyIp = '192.168.15.3';
+            var proxyPort = 8080;
+
+            Memory.writeU16(addrPtr.add(2), ((proxyPort >> 8) & 0xFF) | ((proxyPort & 0xFF) << 8));
+            var proxyIpParts = proxyIp.split('.');
+            Memory.writeU8(addrPtr.add(4), parseInt(proxyIpParts[0]));
+            Memory.writeU8(addrPtr.add(5), parseInt(proxyIpParts[1]));
+            Memory.writeU8(addrPtr.add(6), parseInt(proxyIpParts[2]));
+            Memory.writeU8(addrPtr.add(7), parseInt(proxyIpParts[3]));
+
+            console.log('Redirecionado para ' + proxyIp + ':' + proxyPort);
+        }
+    }
+});
